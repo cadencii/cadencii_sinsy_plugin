240e8d466685525a6507e26f275defdd09d9f15a
kbinani <kbinani.bt@gmail.com>
2014-01-12 17:36:11 +0900

    Fix test errors in MSVC

--- test/stub/ScoreWritableStub.hpp
+++ test/stub/ScoreWritableStub.hpp
@@ -1,74 +1,94 @@
+#ifdef _MSC_VER
+    static inline std::string methodName(std::string const& name)
+    {
+        auto collon = name.rfind("::");
+        if (collon == std::string::npos) {
+            return name;
+        } else {
+            auto begin = collon + 2;
+            auto end = name.rfind("(");
+            return name.substr(begin, end);
+        }
+    }
+    #define __METHOD_NAME__ methodName(__FUNCTION__)
+#else
+    #define __METHOD_NAME__ std::string(__FUNCTION__)
+#endif
+
+
 class ScoreWritableStub : public sinsy::IScoreWritable
 {
 public:
    	void setEncoding(std::string const& encoding) override
    	{
-   		pushResult(__FUNCTION__, encoding);
+        pushResult(__METHOD_NAME__, encoding);
    	}
  
-	void changeTempo(double tempo) override
-	{
-		pushResult(__FUNCTION__, tempo);
-	}
+    void changeTempo(double tempo) override
+    {
+        pushResult(__METHOD_NAME__, tempo);
+    }
 
-	void changeBeat(sinsy::Beat const& beat) override
-	{
-		pushResult(__FUNCTION__, beat);
-	}
+    void changeBeat(sinsy::Beat const& beat) override
+    {
+        pushResult(__METHOD_NAME__, beat);
+    }
 
-	void changeDynamics(sinsy::Dynamics const& dynamics) override
-	{
-		pushResult(__FUNCTION__, dynamics);
-	}
+    void changeDynamics(sinsy::Dynamics const& dynamics) override
+    {
+        pushResult(__METHOD_NAME__, dynamics);
+    }
 
-	void changeKey(sinsy::Key const& key) override
-	{
-		pushResult(__FUNCTION__, key);
-	}
+    void changeKey(sinsy::Key const& key) override
+    {
+        pushResult(__METHOD_NAME__, key);
+    }
 
-	void startCrescendo() override
-	{
-		pushResult(__FUNCTION__);
-	}
+    void startCrescendo() override
+    {
+        pushResult(__METHOD_NAME__);
+    }
 
-	void startDiminuendo() override
-	{
-		pushResult(__FUNCTION__);
-	}
+    void startDiminuendo() override
+    {
+        pushResult(__METHOD_NAME__);
+    }
 
-	void stopCrescendo() override
-	{
-		pushResult(__FUNCTION__);
-	}
+    void stopCrescendo() override
+    {
+        pushResult(__METHOD_NAME__);
+    }
 
-	void stopDiminuendo() override
-	{
-		pushResult(__FUNCTION__);
-	}
+    void stopDiminuendo() override
+    {
+        pushResult(__METHOD_NAME__);
+    }
 
-	void addNote(sinsy::Note const& note) override
-	{
-        pushResult(__FUNCTION__, note);
-	}
+    void addNote(sinsy::Note const& note) override
+    {
+        pushResult(__METHOD_NAME__, note);
+    }
 
-	std::vector<std::string> getResult() const
-	{
-		return result_;
-	}
+    std::vector<std::string> getResult() const
+    {
+        return result_;
+    }
 
 private:
 	template<class T>
-	void pushResult(std::string const& func_name, T const& v)
-	{
+    void pushResult(std::string const& func_name, T const& v)
+    {
 		std::ostringstream s;
 		s << func_name << ":" << v;
 		result_.push_back(s.str());
 	}
 
-	void pushResult(std::string const& func_name)
+    void pushResult(std::string const& func_name)
 	{
 		result_.push_back(func_name);
-	}
+    }
 
 	std::vector<std::string> result_;
 };
+
+#undef __METHOD_NAME__
